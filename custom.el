b8d454685c5445505ba210d0c103624b1492df4e
Oleg Romanenko <oleginga@gmail.com>
2021-05-06 18:08:04 +0300

    Org files added to agenda

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(org-agenda-files
+   '("/home/oleg/Dropbox/org/Церковь.org" "/home/oleg/Dropbox/org/СОЗО.org" "/home/oleg/Dropbox/org/Личное.org" "/home/oleg/Dropbox/org/Книги.org" "/home/oleg/Dropbox/org/Воскресные Служения.org" "/home/oleg/Dropbox/org/bibliography.org" "/home/oleg/Dropbox/org/Начало работы с Orgzly.org" "/home/oleg/Dropbox/org/Начало.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
